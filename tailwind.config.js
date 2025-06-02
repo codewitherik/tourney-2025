1df84d230c71ed3832146b70decdebadae11c9ef
codewitherik <codewitherik>
2025-06-02 20:50:34 +0200

    Tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#007bff',
+        indigo: '#6610f2',
+        purple: '#6f42c1',
+        pink: '#e83e8c',
+        red: '#dc3545',
+        orange: '#fd7e14',
+        yellow: '#ffc107',
+        green: '#28a745',
+        teal: '#20c997',
+        cyan: '#17a2b8',
+        white: '#ffffff',
+        gray: '#6c757d',
+        'gray-dark': '#343a40',
+        primary: '#f04037',
+        secondary: '#5454d4',
+        success: '#28a745',
+        info: '#17a2b8',
+        warning: '#ffc107',
+        danger: '#dc3545',
+        light: '#f8f9fa',
+        dark: '#343a40',
+      },
+      screens: {
+        'xs': '0px',
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+      },
+    },
   },
   plugins: [],
 }
