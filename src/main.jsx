7d1694902c6a928ee03edfe76175510fb9181154
codewitherik <codewitherik>
2025-06-04 21:27:18 +0200

    Ordnersturktur und Komponenten

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,8 @@
-
+import './assets/css/index.css';
+import './assets/css/variables.css';
+import './assets/css/base.css';
+import './assets/css/typography.css';
+import './assets/css/layout.css';
+import './assets/css/animations.css';
+import './assets/css/components.css';
+import './assets/scss/main.scss';
